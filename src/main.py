dd9b48b11f7caf15a921743c338d16666270a40b
Steven Sinakhot <sinakhot.steven@gmail.com>
2025-03-14 01:59:55 +0000

    added vscode python paths

--- src/main.py
+++ src/main.py
@@ -1,24 +1,21 @@
-#    Copyright 2020 June Hanabi
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-
 import sims4.commands
+import services
 
-# This is just a Hello World script, you can play around with it, do a test compile/decompile/debug, or just delete it
-# and start from scratch. The world is yours.
-
-
-@sims4.commands.Command('hellow', command_type=sims4.commands.CommandType.Live)
-def _hellow(_connection=None):
+@sims4.commands.Command('noai', command_type=sims4.commands.CommandType.Live)
+def noai(_connection=None):
     output = sims4.commands.CheatOutput(_connection)
     output("This is my first script mod")
+    sim_info_manager = services.sim_info_manager()
+    active_sim_info = sim_info_manager.get_active_sim_info()
+    if active_sim_info is not None:
+        output(f"Active Sim: {active_sim_info}")
+    else:
+        output("No active Sim found")
+
+    # Disable autonomy for all Sims
+    autonomy_service = services.autonomy_service()
+    if autonomy_service is not None:
+        autonomy_service.set_all_sims_autonomy_enabled(False)
+        output("Autonomy has been disabled for all Sims")
+    else:
+        output("Failed to disable autonomy")
