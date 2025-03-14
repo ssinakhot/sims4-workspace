c0f045c8a6b90f1fcd2e43fc58866aee61a856e2
Steven Sinakhot <sinakhot.steven@gmail.com>
2025-03-14 02:02:27 +0000

    added back in hello world

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import sims4.commands
 import services
 
-@sims4.commands.Command('noai', command_type=sims4.commands.CommandType.Live)
-def noai(_connection=None):
+@sims4.commands.Command('helloworld', command_type=sims4.commands.CommandType.Live)
+def helloworld(_connection=None):
     output = sims4.commands.CheatOutput(_connection)
     output("This is my first script mod")
     sim_info_manager = services.sim_info_manager()
@@ -11,11 +11,3 @@ def noai(_connection=None):
         output(f"Active Sim: {active_sim_info}")
     else:
         output("No active Sim found")
-
-    # Disable autonomy for all Sims
-    autonomy_service = services.autonomy_service()
-    if autonomy_service is not None:
-        autonomy_service.set_all_sims_autonomy_enabled(False)
-        output("Autonomy has been disabled for all Sims")
-    else:
-        output("Failed to disable autonomy")
